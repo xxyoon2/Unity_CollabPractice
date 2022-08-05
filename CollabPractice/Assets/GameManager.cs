0c1e87cecadcffd53d2caf1811ab372a0e967272
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-05 16:17:42 +0900

    Add: LoadSceneAsync

---
+++ CollabPractice/Assets/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(4, LoadSceneMode.Additive);
+    }
+}
