d85142768081b81a7eb14bde439836dba92c49b0
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-05 16:32:44 +0900

    ..

--- CollabPractice/Assets/GameManager.cs
+++ CollabPractice/Assets/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
         SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
